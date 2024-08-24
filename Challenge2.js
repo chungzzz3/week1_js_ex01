03a86f9ff6865a44e1006366377c51a8da26d83f
dinhchung2102 <vodinhchung4@gmail.com>
2024-08-24 10:25:26 +0700

    finish challenge2

--- Challenge2.js
+++ Challenge2.js
@@ -1,3 +1,13 @@
+// Mark and John are trying to compare their BMI (Body Mass Index), which is 
+// calculated using the formula: 
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg 
+// and height in meter). 
+// Your tasks: 
+// 1. Store Mark's and John's mass and height in variables 
+// 2. Calculate both their BMIs using the formula (you can even implement both 
+// versions) 
+// 3. Create a Boolean variable 'markHigherBMI' containing information about 
+// whether Mark has a higher BMI than John.
 
 let markMass = 78; 
 let markHeight = 1.69; 
@@ -5,14 +15,8 @@ let markHeight = 1.69;
 let johnMass = 95; 
 let johnHeight = 1.88; 
 
-
 let markBMI = markMass / (markHeight ** 2);
 let johnBMI = johnMass / (johnHeight ** 2);
 
-if(markBMI > johnBMI){
-    console.log("Mark's BMI higher" )
-} else if(johnBMI > markBMI){
-    console.log("johnMBI higher")
-}else{
-    console.log("Same")
-}
+var markHigherBMI = markBMI > johnBMI
+console.log(`Mark's BMI is higher:  ${markHigherBMI}`);
